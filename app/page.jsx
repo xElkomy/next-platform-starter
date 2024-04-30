0a2d2d4f415a123513f8c31fee77b262efb5b46b
Khaled Mohamed <46958133+xElkomy@users.noreply.github.com>
2024-04-30 21:07:00 +0300

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -10,19 +10,15 @@ const cards = [
 ];
 
 const contextExplainer = `
-The card below is rendered on the server based on the value of \`process.env.CONTEXT\` 
-([docs](https://docs.netlify.com/configure-builds/environment-variables/#build-metadata)):
+This is just a Proof of concept from bugswagger.com about knowing this site is vulnerable to subdomain takeover and bugswagger team take it to make a poc to you.
 `;
 
 const preDynamicContentExplainer = `
-The card content below is fetched by the client-side from \`/quotes/random\` (see file \`app/quotes/random/route.js\`) with a different quote shown on each page load:
+
 `;
 
 const postDynamicContentExplainer = `
-On Netlify, Next.js Route Handlers are automatically deployed as [Serverless Functions](https://docs.netlify.com/functions/overview/).
-Alternatively, you can add Serverless Functions to any site regardless of framework, with acccess to the [full context data](https://docs.netlify.com/functions/api/).
 
-And as always with dynamic content, beware of layout shifts & flicker! (here, we aren't...)
 `;
 
 const ctx = getNetlifyContext();
